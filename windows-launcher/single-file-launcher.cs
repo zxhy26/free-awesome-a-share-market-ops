99e41c0cf0501e683a0112daf8a629abf2b79be4
大A后勤部 <opensource@users.noreply.github.com>
2026-07-28 10:41:34 +0800

    Fix quantitative page launcher release

--- windows-launcher/single-file-launcher.cs
+++ windows-launcher/single-file-launcher.cs
@@ -23,25 +23,25 @@ using System.Windows.Forms;
 #endif
 [assembly: AssemblyCompany("Free & Awesome A-Share Market Ops")]
 [assembly: AssemblyCopyright("Copyright 2026")]
-[assembly: AssemblyVersion("2.15.1.0")]
-[assembly: AssemblyFileVersion("2.15.1.0")]
+[assembly: AssemblyVersion("2.15.2.0")]
+[assembly: AssemblyFileVersion("2.15.2.0")]
 
 internal static class Program
 {
 #if BASIC_EDITION
     private const string EditionName = "基础版";
     private const string EditionCode = "basic";
-    private const string RuntimeTag = "版本_20260728-2.15.1-基础版";
+    private const string RuntimeTag = "版本_20260728-2.15.2-基础版";
     private const string MutexName = "Local\\AshareReviewLauncher_Basic_21400";
 #elif SELF_EDITION
     private const string EditionName = "自用版";
     private const string EditionCode = "self";
-    private const string RuntimeTag = "版本_20260728-2.15.1-自用版";
+    private const string RuntimeTag = "版本_20260728-2.15.2-自用版";
     private const string MutexName = "Local\\AshareReviewLauncher_Self_21400";
 #else
     private const string EditionName = "会员版";
     private const string EditionCode = "member";
-    private const string RuntimeTag = "版本_20260728-2.15.1-会员版";
+    private const string RuntimeTag = "版本_20260728-2.15.2-会员版";
     private const string MutexName = "Local\\AshareReviewLauncher_Member_21400";
 #endif
     private const string PayloadResource = "AshareReviewPayload";
